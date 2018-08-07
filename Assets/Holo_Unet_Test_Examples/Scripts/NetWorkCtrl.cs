e2b336b1b8fc7fff7c77a0bcfd4f0aa0dd06fe5a
StefanSun <821041976@qq.com>
2018-08-07 16:40:35 +0800

    Initial commit

---
+++ Assets/Holo_Unet_Test_Examples/Scripts/NetWorkCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetWorkCtrl : MonoBehaviour {
+    NetworkManager networkManager;
+    NetworkClient netClient;
+	// Use this for initialization
+	void Start () {
+        networkManager = GetComponent<NetworkManager>();
+    }
+
+    public void StartHost()
+    {
+        Debug.Log("StartingHost");
+        networkManager.StartHost();
+        Debug.Log("StartHost");
+    }
+
+    public void StartClient()
+    {
+        netClient = networkManager.StartClient();
+    }
+
+    public void StopClient()
+    {
+        networkManager.StopClient();
+    }
+
+    public void StopHost()
+    {
+        networkManager.StopHost();
+    }
+}
---
+++ Assets/Holo_Unet_Test_Examples/Scripts/PlayerCtrl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerCtrl : NetworkBehaviour
+{
+    public float rotSpeed = 150.0f;
+    public float movSpeed = 3.0f;
+
+    private float x, z;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed;
+        z = Input.GetAxis("Vertical") * Time.deltaTime * movSpeed;
+        transform.Rotate(0,x,0);
+        transform.Translate(0, 0, z);
+    }
+}
