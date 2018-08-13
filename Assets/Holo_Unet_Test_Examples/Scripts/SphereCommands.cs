3064d666599e7de61034222c46a1744bf48a8f23
StefanSun <821041976@qq.com>
2018-08-13 17:36:38 +0800

    08/13 更新
    hololens无法链接到主机的服务器

--- Assets/Holo_Unet_Test_Examples/Scripts/SphereCommands.cs
+++ Assets/Holo_Unet_Test_Examples/Scripts/SphereCommands.cs
@@ -28,17 +28,13 @@ public class SphereCommands : MonoBehaviour
     }
 
     // Called by GazeGestureManager when the user performs a Select gesture
-    void OnSelect()
+    public void OnSelect()
     {
         // If the sphere has no Rigidbody component, add one to enable physics.
         if (!this.GetComponent<Rigidbody>())
         {
             var rigidbody = this.gameObject.AddComponent<Rigidbody>();
             rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
-            if(this.gameObject.name=="Client")
-            {
-                GameObject.Find("NetWorkManager").SendMessage("StartClient");
-            }
         }
     }
 
